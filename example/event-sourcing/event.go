25086453295b18696b81b41b26f6fa92dc98df04
Jesper Hammarbäck <jesper@hammarback.eu>
2019-07-14 15:58:49 +0200

    Added OrderID field to all events.

--- example/event-sourcing/event.go
+++ example/event-sourcing/event.go
@@ -3,25 +3,30 @@ package main
 type OrderEvent interface{}
 
 type OrderPlacedEvent struct {
+	OrderID    OrderID    `json:"orderId"`
 	CustomerID CustomerID `json:"customerId"`
 	Amount     Amount     `json:"amount"`
 }
 
 type PaymentReceivedEvent struct {
+	OrderID    OrderID    `json:"orderId"`
 	CustomerID CustomerID `json:"customerId"`
 	AmountPaid Amount     `json:"amountPaid"`
 }
 
 type OrderFullyPaidEvent struct {
+	OrderID    OrderID    `json:"orderId"`
 	CustomerID CustomerID `json:"customerId"`
 }
 
 type OrderCancelledEvent struct {
+	OrderID    OrderID    `json:"orderId"`
 	CustomerID CustomerID `json:"customerId"`
 	Reason     string     `json:"reason"`
 }
 
 type OrderShippedEvent struct {
+	OrderID        OrderID        `json:"orderId"`
 	CustomerID     CustomerID     `json:"customerId"`
 	TrackingNumber TrackingNumber `json:"trackingNumber"`
 }
